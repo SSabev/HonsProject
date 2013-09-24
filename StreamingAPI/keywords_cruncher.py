8f83a123e73a6daf435e78a652bb64b18c572aac
Stefan Sabev <subevstefan@gmail.com>
2013-09-24 14:52:46 +0100

    changing some small stuff

---
+++ StreamingAPI/keywords_cruncher.py
@@ -0,0 +1,45 @@
+import re
+import json
+from ast import literal_eval
+import datetime 
+
+class KeyStats(object):
+    
+    def __init__(self, filename):
+
+        self.filehandler = open(filename, 'rb')
+        self.starttime = datetime.datetime.now()
+        self.hashtags = {}
+        self.get_keywords()
+        # self.something_else
+    
+    def get_keywords(self):
+        for line in self.filehandler:
+            data = literal_eval(line)
+            if 'entities' in data:
+                hastags = data['entities']['hashtags']
+                for hashtag in hastags:
+                    text = hashtag['text']
+                    self.hashtags[text] = self.hashtags.get(text, 0)
+                    self.hashtags[text] +=1
+
+    def dump_tags(self):
+
+        print self.hashtags
+
+
+if __name__ == '__main__':
+
+    tags = KeyStats('Data-23.9.2013')
+
+    # tags.dump_tags()
+    delta = datetime.datetime.now() - tags.starttime
+    
+    outfile = open('outfile', 'wb')
+
+
+    outfile.write(json.dumps(tags.hashtags, indent=4))
+    outfile.write(json.dumps(sorted(tags.hashtags.items(), key=lambda x: x[1]), indent=4))
+    outfile.close()
+
+    print delta.seconds 
