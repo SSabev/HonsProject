58a3711f5f770de649069678f773c596fb96951d
Stefan Sabev <stefan.sabev@skyscanner.net>
2013-11-01 12:03:14 +0000

    adding a base aggregator
    Which doesn't work on both OS X and Windows. It seems that OS X
    segfaults when reading csv and Windows just can't handle that much...

--- StreamingAPI/aggregator.py
+++ StreamingAPI/aggregator.py
@@ -11,13 +11,27 @@ class Aggregator(object):
 
         self.filenames = filenames
         self.load()
-            
+        self.get_tags()
+        self.get_popular()
+
     def load(self):
         dataframes = []
         for i in self.filenames:
             dataframes.append(p.read_csv(i))
 
         self.data = p.concat(dataframes)
+        print self.data
+
+    def get_tags(self):
+        print len(self.data['Hashtag'])
+        print len(set(self.data['Hashtag']))
+
+    def get_popular(self):
+
+        df = self.data.groupby('Hashtag')
+        df.sum()
+
+        df.to_csv('temp.csv')
 
 if __name__ == '__main__':
     all_the_files = [i for i in glob.glob(r'processed/*.csv')]
