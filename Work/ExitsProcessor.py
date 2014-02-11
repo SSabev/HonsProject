2a8927278fd2d736354c16c78c0c7513b2a883bc
Stefan Sabev <subevstefan@gmail.com>
2014-02-11 16:49:19 +0000

    And the log from the tidifier

--- Work/ExitsProcessor.py
+++ Work/ExitsProcessor.py
@@ -26,10 +26,8 @@ class ExitsProcessor(object):
     
     def load_single(self):
         self.data = p.read_csv(self.filenames)
-        del self.data['ToAirport']
-        del self.data['ToCountry']
+        self.original = self.data
         print self.data
-
 
     def list_cities(self):
         print len(self.data['ToCity'])
@@ -38,8 +36,10 @@ class ExitsProcessor(object):
         
     def make_extracts_for_cities(self):
         bad, good = 0, 0
+        del self.data['ToAirport']
+        del self.data['ToCountry']
         for i in iter(self.uniques):
-            if (i in self.cities or i in terms.terms or i in terms.countries):
+            if (i in self.cities):
                 df = self.data[self.data['ToCity'] == i] 
                 if sum(df['Exits'])>100:
                     df = df.groupby(['Date','ToCity'])
@@ -70,9 +70,47 @@ class ExitsProcessor(object):
         print "%s finished successfully"%str(good)
         print "%s went tits up"%str(bad)
 
+    def make_extracts_for_countries(self):
+        bad, good = 0, 0
+        del self.original['ToAirport']
+        del self.original['ToCity']
+        for i in iter(set(self.original['ToCountry'])):
+            print i
+            df = self.original[self.original['ToCountry'] == i] 
+            if sum(df['Exits'])>100:
+                df = df.groupby(['Date','ToCountry'])
+                new_df = df.agg({'Searches': np.sum, 'Exits': np.sum}).reset_index()
+                del new_df['ToCountry']
+                new_df.sort('Date',ascending=True, inplace=True) 
+                try:
+                    st = dt.datetime.strptime('2013-10-30', "%Y-%m-%d")
+                    en = st + relativedelta(days = 10)
+                    while st < en:
+                        temp = st.strftime('%Y-%m-%d')
+                        new_df = new_df[new_df.Date != temp]
+                        st += relativedelta(days = 1)
+                    
+                    new_df = new_df[new_df.Date != '2013-12-12']
+                    a = Analyser(new_df)
+                    a.backfill('Searches')
+                    a.backfill('Exits')
+                    a.results.Searches = a.results.Searches.astype('int')
+                    a.results.Exits = a.results.Exits.astype('int')
+                    del a.results['Forecast']
+                    a.results.to_csv('%s/%s.csv'%(self.directory,i), index=False)
+                    #print "Finished successfully %s"%self.cities[i]
+                    good += 1
+                except TypeError:
+                    bad += 1
+
+        print "%s finished successfully"%str(good)
+        print "%s went tits up"%str(bad)
+
 if __name__ == '__main__':
     f = 'searches.csv'
     directory = 'tidydata/se'
     a = ExitsProcessor(f, directory)
     a.list_cities()
-    a.make_extracts_for_cities()
+    #a.make_extracts_for_cities()
+    a.make_extracts_for_countries()
+
