7da937ade844b5388decf340bf2bc9b6ea300c66
Stefan Sabev <subevstefan@gmail.com>
2014-02-17 19:35:05 +0000

    Some new plots

--- Work/plotting.py
+++ Work/plotting.py
@@ -3,25 +3,41 @@ import pandas as p
 import datetime as dt
 import glob
 
-def plot_city(city):
+def plot_both(city):
     """
     Simple small script for plotting timeseries
     """
+    try:
+        data = p.read_csv('tidydata/joined/%s.csv'%city)
 
-    data = p.read_csv('tc/%s.csv'%city)
+        def apply_dt(row):
+            if len(row) > 10:
+                return dt.datetime.strptime(row, '%Y-%m-%d %H:%M:%S')
+            else:
+                return dt.datetime.strptime(row, '%Y-%m-%d')
+        data['Date'] = data['Date'].apply(apply_dt) 
 
-    data['Datetime'] = data['Datetime'].map(lambda x: dt.datetime.strptime(x,'%Y-%m-%d'))
-    plt.title('Mentions of %s on Twitter'%city)
-    plt.plot(data['Datetime'], data['Count'])
-    plt.show()
+        plt.figure(1)
+        plt.subplot(211)
+        plt.title('Mentions of %s on Twitter'%city)
+        plt.plot(data['Date'], data['Count'])
+
+        plt.subplot(212)
+        plt.title('Searches to %s'%city)
+        plt.plot(data['Date'], data['NSearches'])
+        plt.show()
+    except IOError:
+        print "no such place"
+
+    
 
 def plot_exits(city):
     """
     This bit here will plot the exits/searches
     """
-    data = p.read_csv('Exits/%s.csv'%city)
+    data = p.read_csv('tidydata/se/%s.csv'%city)
 
-    data['Date'] = data['Date'].map(lambda x: dt.datetime.strptime(x,'%Y-%m-%d'))
+    data['Date'] = data['Date'].map(lambda x: dt.datetime.strptime(x,'%Y-%m-%d %H:%M%:%S'))
     data['Exits'] = data['Exits'].map(lambda x: int(x))
 
     plt.plot(data['Date'], data['Exits'])
@@ -29,15 +45,15 @@ def plot_exits(city):
 
 
 if __name__ == '__main__':
-    keyword_files = [i for i in glob.glob('tc/*csv')]
+    keyword_files = [i for i in glob.glob('tidydata/joined/*csv') if 'London' in i]
     top_n = []
     for i in keyword_files:
         data = p.read_csv(i)
         top_n.append((sum(data['Count']), i.split('/')[-1].replace('.csv','')))
 
     top_n = sorted(top_n, reverse=True)
-    for i, j in top_n[:12]:
+    for i, j in top_n[:20]:
         print "%s %s"%(j, i)
-        plot_city(j)
+        plot_both(j)
 
 
