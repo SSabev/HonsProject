9fc428eb6aad84082d66ab6cb8e624c46355c94b
Stefan Sabev <subevstefan@gmail.com>
2015-02-11 13:02:33 +0000

    regular commit

--- Work/Rscripts/online_changepoint.R
+++ Work/Rscripts/online_changepoint.R
@@ -4,6 +4,7 @@ library(zoo)
 library("TTR")
 library('hydroGOF')
 library('changepoint')
+library('reshape')
 setwd('Dev/HonsProject/Work/Rscripts')
 
 # Get all the errors
@@ -21,7 +22,7 @@ for(i in 30:430){
   forecast <- forecast.Arima(arima, h=1)
   predicted_vals <- forecast$mean
   actual_vals <- place_ts[i:i+1]
-  error <- predicted_vals - actual_vals
+  error <- as.integer(predicted_vals - actual_vals)
   writeLines(paste(i, " ", error))
   changepoint <- 0
   
@@ -29,19 +30,25 @@ for(i in 30:430){
   if(i > 35){
     df_arima <- df_arima[complete.cases(df_arima), ]
     num <- i - 30
-    writeLines(paste("WE have x lines" , num))
     dt <- data.frame(x=c(1:num),y=df_arima$Error)
     dens <- density(dt$y)
     df <- data.frame(x=dens$x, y=dens$y)
-    probs <- c(0.97, 0.98, 0.99)
+    probs <- c(0.005, 0.01,  0.99, 0.995)
     quantiles <- quantile(dt$y, prob=probs)
     df$quant <- factor(findInterval(df$x,quantiles))
     ggplot(df, aes(x,y)) + geom_line() + geom_ribbon(aes(ymin=0, ymax=y, fill=quant)) + scale_x_continuous(breaks=quantiles) + scale_fill_brewer(guide="none")
+    partition <- 4
+    inv_partition <- 1
     
+    if( i - 300 > 100){
+      partition <- 3
+      inv_partition <- 2
+    }
     
-    parition <- 4 - as.integer(i/133)
-    writeLines(paste("The quanttile we're looking at is ", quantiles[partition]))
-    if (error > quantiles[partition]){
+    writeLines(paste("The quanttile we're looking at is ", quantiles))
+    writeLines(paste("The mean of the error is " , mean(df_arima$Error), " and the stdev is ", sd(df_arima$Error) ))
+    writeLines(paste(partition, " and ", inv_partition))
+    if (error > quantiles[partition] || error < quantiles[inv_partition]){
       writeLines('CHANGEPOINT')
       changepoint <- 1
     }
