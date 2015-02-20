424a3a9e1cb3dfc72660399e9fe105442deab83a
Stefan Sabev <subevstefan@gmail.com>
2015-02-20 19:40:12 +0000

    update

--- Work/Rscripts/online_changepoint.R
+++ Work/Rscripts/online_changepoint.R
@@ -17,7 +17,14 @@ place_ts <- ts(data$Searches)
 plot.ts(place_ts)
 df_arima <- data.frame(Predicted=NA, Actual=NA, Error=NA, ChangePoint=NA)
 
-for(i in 30:430){
+n <- 0
+mean <- 0
+M2 <- 0
+numstdevs <- 4
+changepoint_decay <-5 
+found <- FALSE
+
+for(i in 30:429){
   arima <- arima(place_ts[1:i], order=c(4,1,5), method='ML')
   forecast <- forecast.Arima(arima, h=1)
   predicted_vals <- forecast$mean
@@ -26,32 +33,41 @@ for(i in 30:430){
   writeLines(paste(i, " ", error))
   changepoint <- 0
   
+  n <- n + 1
+  delta <- error - mean
+  mean <- mean + delta/n
+  M2 <- M2 + delta*(error - mean)
+  variance <- M2/(n - 1)
+  stdev <- sqrt(variance)
   
+  if(i > 250){
+    numstdevs <- 3
+  }
+  
+
   if(i > 35){
     df_arima <- df_arima[complete.cases(df_arima), ]
     num <- i - 30
     dt <- data.frame(x=c(1:num),y=df_arima$Error)
     dens <- density(dt$y)
     df <- data.frame(x=dens$x, y=dens$y)
-    probs <- c(0.005, 0.01,  0.99, 0.995)
-    quantiles <- quantile(dt$y, prob=probs)
-    df$quant <- factor(findInterval(df$x,quantiles))
-    ggplot(df, aes(x,y)) + geom_line() + geom_ribbon(aes(ymin=0, ymax=y, fill=quant)) + scale_x_continuous(breaks=quantiles) + scale_fill_brewer(guide="none")
-    partition <- 4
-    inv_partition <- 1
-    
-    if( i - 300 > 100){
-      partition <- 3
-      inv_partition <- 2
-    }
-    
-    writeLines(paste("The quanttile we're looking at is ", quantiles))
-    writeLines(paste("The mean of the error is " , mean(df_arima$Error), " and the stdev is ", sd(df_arima$Error) ))
-    writeLines(paste(partition, " and ", inv_partition))
-    if (error > quantiles[partition] || error < quantiles[inv_partition]){
+    writeLines(paste("Online mean is ", mean, " and online stdev is ", stdev))
+#     if(found && changepoint_decay > 1){
+#       changepoint_decay <- changepoint_decay - 1
+#     }
+#     
+#     if(changepoint_decay == 1){
+#       changepoint_decay <- 5
+#       found <- FALSE
+#     }
+#     
+    if (error > mean + numstdevs*stdev || error < mean - numstdevs*stdev || !found){
       writeLines('CHANGEPOINT')
       changepoint <- 1
+      found <- TRUE
     }
+    
+
   }
   
   df_arima <- rbind(df_arima, c(predicted_vals, actual_vals, error, changepoint))
